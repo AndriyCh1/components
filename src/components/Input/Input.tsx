103a6112c2bdafad54e990acb932f5fba12d2f11
AndriyCh1 <chekanovskiyacc@gmail.com>
2024-07-12 18:22:52 +0300

    controllable Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,23 +1,38 @@
-import { FC, InputHTMLAttributes } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 
 import css from "./Input.module.css";
 
-export interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface IProps {
+  className?: string;
   label?: string;
+  value?: string;
+  onChange?: (value: string) => void;
 }
 
 export const Input: FC<IProps> = (props) => {
-  const { label, className, ...attributes } = props;
+  const { label, className, onChange, ...attributes } = props;
+
+  const [value, setValue] = useState("");
 
   const inputAttributes = {
     ...attributes,
     className: [className, css.input].join(" "),
   };
 
+  useEffect(() => {
+    if (onChange) {
+      onChange(value);
+    }
+  }, [value]);
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div>
       {label && <label className={css.label}>{label}</label>}
-      <input {...inputAttributes} />
+      <input {...inputAttributes} onChange={handleOnChange} />
     </div>
   );
 };
